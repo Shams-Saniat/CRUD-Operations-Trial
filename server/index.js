f20c1889ed1c4b75a1f8da80465fed3f71e8a925
shamssaniat <shams.saniat@northsouth.edu>
2025-04-02 00:40:05 +0600

    recorded a mew API

--- server/index.js
+++ server/index.js
@@ -8,8 +8,14 @@ app.use(cors())
 app.use(express.json())
 
 
-mongoos.connect("mongodb://127.0.0.1:27017/crud")
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
 
-app.listen(3001, ()=> {
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
     console.log("Server is Running");
 })
